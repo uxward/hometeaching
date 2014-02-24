ed49aa5fa8576e7d1b1bdfdc96833bac677cb048
Phillip Manwaring <philman311@gmail.com>
2014-02-24 10:29:10 -0600

    update db change sql

--- db/update-email.sql
+++ db/update-email.sql
@@ -1,5 +1,6 @@
 alter table family add familymoved bit;
 alter table family add recordsmoved bit;
+alter table family add partmember bit;
 
 update family set familymoved = false where familymoved is null;
 update family set recordsmoved = false where recordsmoved is null;
