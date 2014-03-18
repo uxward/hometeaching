a28169267d93ca0a9742a313c845a16c0b95d25c
Phillip Manwaring <philman311@gmail.com>
2014-03-18 16:42:57 -0500

    update email template reference

--- src/main/webapp/resources/js/google-analytics.js
+++ src/main/webapp/resources/js/google-analytics.js
@@ -3,5 +3,5 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-46748553-1', 'cloudbees.net');
+ga('create', 'UA-46748553-2', 'hometeaching.elasticbeanstalk.com');
 ga('send', 'pageview');
